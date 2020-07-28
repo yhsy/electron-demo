ab0c9b676cdb1dbf5bfdfef84314db730734c23c
juerui <juerui@vip.qq.com>
2020-07-28 11:38:03 +0800

    菜单栏-显示隐藏菜单栏(主进程和渲染之间相互通信)

--- demo15/main.js
+++ demo15/main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, shell, Menu } = require("electron");
+const { app, BrowserWindow, shell, Menu, ipcMain } = require("electron");
 
 let mainWin = null;
 
-app.on("ready", () => {
+app.on("ready", createWindow);
+
+// 默认主窗口
+function createWindow() {
   mainWin = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -20,8 +23,8 @@ app.on("ready", () => {
     show: false,
   });
 
-  // 自定义菜单栏
-  require("./main/teambition.js");
+  // 创建菜单栏(自定义)
+  createMenu();
 
   mainWin.loadFile("index.html");
 
@@ -34,4 +37,200 @@ app.on("ready", () => {
     mainWin = null;
     app.quit();
   });
+}
+
+// 自定义菜单
+function createMenu() {
+  const menuTemplate = [
+    {
+      label: "文件",
+      submenu: [
+        {
+          label: "偏好设置",
+          accelerator: "ctrl + ,",
+          icon: "./assets/icon/icon-setting.png",
+          enabled: false,
+        },
+        // 分割线
+        {
+          type: "separator",
+        },
+        {
+          label: "退出",
+          accelerator: "ctrl + Q",
+          role: "quit",
+          icon: "./assets/icon/icon-logout.png",
+        },
+      ],
+    },
+    {
+      label: "修改",
+      submenu: [
+        {
+          label: "撤销",
+          role: "undo",
+        },
+        {
+          label: "重做",
+          role: "redo",
+        },
+        // 分割线
+        {
+          type: "separator",
+        },
+        {
+          label: "剪切",
+          role: "cut",
+        },
+        {
+          label: "复制",
+          role: "copy",
+        },
+        {
+          label: "粘贴",
+          role: "paste",
+        },
+        {
+          label: "粘贴并匹配样式",
+          role: "pasteAndMatchStyle",
+        },
+        {
+          label: "全选",
+          role: "selectAll",
+        },
+      ],
+    },
+    {
+      label: "视图",
+      submenu: [
+        {
+          label: "停止",
+          enabled: false,
+        },
+        {
+          label: "重新加载",
+          role: "reload",
+          accelerator: "ctrl + R",
+        },
+        // 分割线
+        {
+          type: "separator",
+        },
+        {
+          label: "放大",
+          role: "zoomin",
+          accelerator: "ctrl + =",
+        },
+        {
+          label: "缩小",
+          role: "zoomout",
+          accelerator: "ctrl + -",
+        },
+        {
+          label: "重置",
+          role: "resetzoom",
+          accelerator: "ctrl + O",
+        },
+        // 分割线
+        {
+          type: "separator",
+        },
+        {
+          label: "切换全屏",
+          role: "togglefullscreen",
+          accelerator: "ctrl + shift + F",
+        },
+      ],
+    },
+    {
+      label: "帮助",
+      submenu: [
+        {
+          label: "隐藏菜单栏",
+          icon: "./assets/icon/icon-hide.png",
+          accelerator: "ctrl + shift + H",
+          click: () => {
+            // 隐藏会闪退窗口(bug)
+            // Menu.setApplicationMenu(null);
+            mainWin.webContents.send("status", 0);
+          },
+          // enabled: false,
+        },
+        {
+          label: "网络自助测试",
+          icon: "./assets/icon/icon-network.png",
+          click: () => {
+            let helpWin = null;
+            helpWin = new BrowserWindow({
+              width: 1000,
+              height: 800,
+              webPreferences: {
+                nodeIntegration: true,
+              },
+            });
+            // 加载远程URL
+            helpWin.loadURL("https://help.teambition.com/");
+
+            helpWin.on("closed", () => {
+              helpWin = null;
+            });
+          },
+        },
+        {
+          label: "检查版本",
+          icon: "./assets/icon/icon-update.png",
+          click: () => {
+            dialog.showMessageBox({
+              type: "info",
+              title: "Teambition",
+              message: "目前版本是V1.13,没有找到新版本",
+            });
+          },
+        },
+        // 分割线
+        {
+          type: "separator",
+        },
+        {
+          label: "更新日志",
+          icon: "./assets/icon/icon-log.png",
+          click: () => {
+            shell.openExternal(
+              "https://dn-st.teambition.net/windows/zh/changelog.html"
+            );
+          },
+        },
+        {
+          label: "调试工具",
+          icon: "./assets/icon/icon-dev.png",
+          role: "toggledevtools",
+        },
+      ],
+    },
+  ];
+
+  const tembitionMenu = Menu.buildFromTemplate(menuTemplate);
+
+  Menu.setApplicationMenu(tembitionMenu);
+}
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// 当应用被激活时(首次启动,任务栏点击图标,)
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// 渲染进程通信(显示菜单栏)
+ipcMain.on("ipcStatus", (event, args) => {
+  // console.log(args);
+  if (args) {
+    createMenu();
+  }
 });
--- demo15/renderer/index.js
+++ demo15/renderer/index.js
@@ -1,6 +1,11 @@
+const { ipcRenderer } = require("electron");
+const { Menu } = require("electron").remote;
+
 window.onload = () => {
   const webview = document.querySelector("webview");
   const mask = document.querySelector(".m-mask");
+  const btnHideMenu = document.querySelector("#btnHideMenu");
+  const btnShowMenu = document.querySelector("#btnShowMenu");
 
   // 页面开始加载
   webview.addEventListener("did-start-loading", () => {
@@ -15,5 +20,25 @@ window.onload = () => {
     // webview设置为可见
     webview.style.display = "inline-flex";
     // console.log(`网页链接:${webview.getURL()}`);
+  });
+
+  // 隐藏菜单栏
+  btnHideMenu.addEventListener("click", () => {
+    Menu.setApplicationMenu(null);
+  });
+
+  // 显示菜单栏
+  btnShowMenu.addEventListener("click", () => {
+    // 发送消息给主进程
+    ipcRenderer.send("ipcStatus", 1);
+  });
+
+  // 接收主进程消息(隐藏菜单栏)
+  ipcRenderer.on("status", (event, args) => {
+    // console.log("主进程消息收到了！");
+    // console.log(args);
+    if (!args) {
+      Menu.setApplicationMenu(null);
+    }
   });
 };

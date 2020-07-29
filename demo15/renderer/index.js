14256a8deb4f23b42101d27361769a40d5b7b60f
juerui <juerui@vip.qq.com>
2020-07-29 16:12:33 +0800

    demo15-右键菜单-前进和后退功能(两次灰色区域有效)

--- demo15/renderer/index.js
+++ demo15/renderer/index.js
@@ -1,62 +1,99 @@
 const { ipcRenderer, remote } = require("electron");
 const { Menu } = remote;
 
+// Dom 选择器(复用)
+const $ = (el) => {
+  // console.log(id);
+  return document.querySelector(el);
+};
+
 var onOff = true;
 
-let rightTemplateHide = [
-  {
-    label: "返回",
-    enabled: false,
-  },
-  {
-    label: "前进",
-    enabled: false,
-  },
-  {
-    label: "重新加载",
-    role: "reload",
-  },
-  {
-    label: "隐藏菜单栏",
-    click: () => {
-      onOff = false;
-      Menu.setApplicationMenu(null);
-    },
-  },
-];
-
-let rightTemplateShow = [
-  {
-    label: "返回",
-    enabled: false,
-  },
-  {
-    label: "前进",
-    enabled: false,
-  },
-  {
-    label: "重新加载",
-    role: "reload",
-  },
-  {
-    label: "显示菜单栏",
-    click: () => {
-      onOff = true;
-      // 发送消息给主进程
-      ipcRenderer.send("ipcStatus", 1);
-    },
-  },
-];
-
-// 生成右键菜单
-let rightMenuHide = Menu.buildFromTemplate(rightTemplateHide);
-let rightMenuShow = Menu.buildFromTemplate(rightTemplateShow);
-
 window.onload = () => {
-  const webview = document.querySelector("webview");
-  const mask = document.querySelector(".m-mask");
-  // const btnHideMenu = document.querySelector("#btnHideMenu");
-  // const btnShowMenu = document.querySelector("#btnShowMenu");
+  const webview = $("webview");
+  const mask = $(".m-mask");
+  // const btnHideMenu = $("#btnHideMenu");
+  // const btnShowMenu = $("#btnShowMenu");
+  // const btnBack = $("#btnBack");
+  // const btnForward = $("#btnForward");
+
+  // 右键菜单(隐藏菜单栏)
+  let rightTemplateHide = [
+    {
+      label: "返回",
+      // enabled: false,
+      click: () => {
+        if (webview.canGoBack()) {
+          webview.goBack();
+        } else {
+          alert("不能返回");
+        }
+      },
+    },
+    {
+      label: "前进",
+      // enabled: false,
+      click: () => {
+        if (webview.canGoForward()) {
+          webview.goForward();
+        } else {
+          alert("不能前进");
+        }
+      },
+    },
+    {
+      label: "重新加载",
+      role: "reload",
+    },
+    {
+      label: "隐藏菜单栏",
+      click: () => {
+        onOff = false;
+        Menu.setApplicationMenu(null);
+      },
+    },
+  ];
+  // 右键菜单(显示菜单栏)
+  let rightTemplateShow = [
+    {
+      label: "返回",
+      // enabled: false,
+      click: () => {
+        if (webview.canGoBack()) {
+          webview.goBack();
+        } else {
+          alert("不能返回");
+        }
+      },
+    },
+    {
+      label: "前进",
+      // enabled: false,
+      click: () => {
+        if (webview.canGoForward()) {
+          webview.goForward();
+        } else {
+          alert("不能前进");
+        }
+      },
+    },
+    {
+      label: "重新加载",
+      role: "reload",
+    },
+    {
+      label: "显示菜单栏",
+      click: () => {
+        onOff = true;
+        // 发送消息给主进程
+        ipcRenderer.send("ipcStatus", 1);
+      },
+    },
+  ];
+
+  // 生成右键菜单
+  let rightMenuHide = Menu.buildFromTemplate(rightTemplateHide);
+  let rightMenuShow = Menu.buildFromTemplate(rightTemplateShow);
 
   // 页面开始加载
   webview.addEventListener("did-start-loading", () => {
@@ -91,17 +128,6 @@ window.onload = () => {
     }
   });
 
-  // 隐藏菜单栏
-  // btnHideMenu.addEventListener("click", () => {
-  //   Menu.setApplicationMenu(null);
-  // });
-
-  // 显示菜单栏
-  // btnShowMenu.addEventListener("click", () => {
-  //   // 发送消息给主进程
-  //   ipcRenderer.send("ipcStatus", 1);
-  // });
-
   // 接收主进程消息(隐藏菜单栏)
   ipcRenderer.on("status", (event, args) => {
     // console.log("主进程消息收到了！");
@@ -110,4 +136,43 @@ window.onload = () => {
       Menu.setApplicationMenu(null);
     }
   });
+
+  // // 隐藏菜单栏
+  // btnHideMenu.addEventListener("click", () => {
+  //   Menu.setApplicationMenu(null);
+  // });
+
+  // // 显示菜单栏
+  // btnShowMenu.addEventListener("click", () => {
+  //   // 发送消息给主进程
+  //   ipcRenderer.send("ipcStatus", 1);
+  // });
+
+  // // 后退
+  // btnBack.addEventListener("click", () => {
+  //   // 判断能否后退
+  //   if (webview.canGoBack()) {
+  //     console.log("能后退");
+  //     // $("#currentUrl").innerHTML = webview.getURL();
+  //     webview.goBack();
+  //   } else {
+  //     console.log("不能后退");
+  //     // $("#currentUrl").innerHTML = webview.getURL();
+  //   }
+  //   // console.log("后退");
+  //   // webview.goBack();
+  //   // console.log(`网页链接:${webview.getURL()}`);
+  //   // const url = webview.getURL();
+  // });
+
+  // // 前进
+  // btnForward.addEventListener("click", () => {
+  //   // 判断能否前进
+  //   if (webview.canGoForward()) {
+  //     console.log("能前进");
+  //     webview.goForward();
+  //   } else {
+  //     console.log("不能前进");
+  //   }
+  // });
 };
